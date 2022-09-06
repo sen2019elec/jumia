28d3370094327d8f80ae8eeb828ccd7be08f039a
camara <sen.elecplomb2019@gmail.com>
2022-09-06 14:07:09 +0000

    god

--- tp3.js
+++ tp3.js
@@ -6,6 +6,8 @@ let nombre_image=image.length;
 function alternanceActiveImage(){
     /*parcour du tableau */
     for(let i=0;i<nombre_image;i++){
+        /*image qui affiche est actique (contains)
+        de le suprimer si image est depasse(remve) */
         if (image[i].classList.contains("active")) {
             image[i].classList.remove('active');
         }
@@ -16,7 +18,7 @@ function alternanceActiveImage(){
 /*recuperation des boutons */
 let envoyer=document.querySelector(".seconde");
 let envoie=document.querySelector(".favorite");
-/*si on clic sur le bouton change moi les images du fonction */
+/*si on clic sur le bouton favirite change moi les images du fonction */
 envoyer.addEventListener("click",function(){
     etape++;
     if(etape >= nombre_image){
@@ -26,6 +28,7 @@ envoyer.addEventListener("click",function(){
     alternanceActiveImage();
     image[etape].classList.add('active');
 })
+/*si on clic sur le bouton seconde change moi les images du fonction */
     envoie.addEventListener("click",function(){
             etape--;
             if(etape<0){
@@ -45,4 +48,8 @@ setInterval(()=>{
     alternanceActiveImage();
     image[etape].classList.add('active');
 },3000);
-
+  
+/*recuperation photo defile */
+let photo=document.querySelectorAll('.bll');
+let etap=0
+let nombre_photo=photo.length;
